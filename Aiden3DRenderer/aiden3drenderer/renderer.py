b1d36960eb3e1a37b63579bc425f9266513a3bd3
AidenKielby <131564693+AidenKielby@users.noreply.github.com>
2026-03-10 16:30:23 -0400

    Renderer: removed raytracing

--- Aiden3DRenderer/aiden3drenderer/renderer.py
+++ Aiden3DRenderer/aiden3drenderer/renderer.py
@@ -30,7 +30,6 @@ class renderer_type(Enum):
     RASTERIZE = "rasterize"
     POLYGON_FILL = "polygon_fill"
     MESH = "mesh"
-    RAYTRACE = "raytrace"
 
 compute_shader_for_rasterization = """
 #version 430
@@ -254,6 +253,8 @@ class Renderer3D:
         self.is_using_default_shapes = load_default_shapes
         self._default_shape_names = set()
         self._default_shapes_loaded = False
+
+        self.lighting_strictness = 0.5
 
         if load_default_shapes:
             before = set(CUSTOM_SHAPES.keys())
@@ -594,6 +595,17 @@ class Renderer3D:
                     0,
                 )
 
+    def smax_poly(self, a, b, k):
+        h = np.clip(0.5 + 0.5 * (b - a) / k, 0.0, 1.0)
+        return self.mix(b, a, h) + k * h * (1.0 - h)
+    
+    def smax_exp(self, a, b, k):
+        return np.log(np.exp(k * a) + np.exp(k * b)) / k
+
+    # Helper function
+    def mix(self, a, b, t):
+        return a * (1.0 - t) + b * t
+
     def render_shape_from_obj_format(self, matrix, texture_p):
         #print(matrix[0][0])
         if self.render_type == renderer_type.RASTERIZE:
@@ -607,6 +619,7 @@ class Renderer3D:
                     continue
                 #print(mat)
                 vertices, faces, uv, uv_faces = mat
+                
                 if self.using_obj_filetype_format:
                     unprojected_verticies, same_faces, same_uv, same_uv_faces = self.vertices_faces_list[matI]
                 for faceI in range(len(faces)):
@@ -625,7 +638,7 @@ class Renderer3D:
 
                     light_dir = np.array([0, 1, 0])
 
-                    light_m = max(0.25, np.dot(light_dir, np.array(unprojected_normal)))
+                    light_m = self.smax_exp(self.lighting_strictness, np.dot(light_dir, np.array(unprojected_normal)), 1)
                     
                     uv_face = uv_faces[faceI]
                     uv0 = uv[uv_face[0]]
@@ -795,144 +808,6 @@ class Renderer3D:
                     pygame.draw.line(self.screen, (0, 0, 0), p1, p2, 2)
                     pygame.draw.line(self.screen, (0, 0, 0), p2, p0, 2)
     
-    def does_ray_intersect(self, ray_origin, step_size, direction, triangle):
-        v0 = triangle[0]
-        v1 = triangle[1]
-        v2 = triangle[2]
-
-        e1 = np.array(v1) - np.array(v0)
-        e2 = np.array(v2) - np.array(v0)
-
-        h = np.array(self.cross(direction, e2.tolist()))
-        det = np.dot(h, e1)
-
-        if det < 0.1 and det > -0.1:
-            return (False, None)
-        
-        t = np.array(ray_origin) - np.array(v0)
-        u = (np.dot(t,h)) / det
-
-        if u < 0 or u > 1:
-            return (False, None)
-        
-        q = np.cross(t,e1)
-        v = (np.dot(np.array(direction), q ) / det)
-
-        if v < 0 or u + v > 1:
-            return (False, None)
-        
-        t = (np.dot(e2, q)) / det
-
-        if t > step_size:
-            return (True, t)
-        
-    def shade(self, hit_point, closest_tri, texture_path):
-        ws = np.array([
-            1.0 / closest_tri['d1'],
-            1.0 / closest_tri['d2'],
-            1.0 / closest_tri['d3']
-        ])
-
-        us = np.array([
-            closest_tri['uv1'][0] * ws[0],
-            closest_tri['uv2'][0] * ws[1],
-            closest_tri['uv3'][0] * ws[2]
-        ])
-        vs = np.array([
-            closest_tri['uv1'][1] * ws[0],
-            closest_tri['uv2'][1] * ws[1],
-            closest_tri['uv3'][1] * ws[2]
-        ])
-
-        u_over_w   = self.depth_in_tri(closest_tri['pos1'], closest_tri['pos2'], closest_tri['pos3'], hit_point, us)
-        v_over_w   = self.depth_in_tri(closest_tri['pos1'], closest_tri['pos2'], closest_tri['pos3'], hit_point, vs)
-        one_over_w = self.depth_in_tri(closest_tri['pos1'], closest_tri['pos2'], closest_tri['pos3'], hit_point, ws)
-
-        uv = np.array([u_over_w / one_over_w, 1.0 - (v_over_w / one_over_w)])
-        
-        tex = Image.open(texture_path).convert("RGBA")
-
-        color = self.sample_texture(tex, uv)
-        return (color[0], color[1], color[2])
-    
-    def sample_texture(self, img, uv):  
-        w, h = img.size
-        x = int(uv[0] * w) % w
-        y = int(uv[1] * h) % h
-        r, g, b = img.getpixel((x, y))[:3]
-        return np.array([r/255, g/255, b/255])
-
-    def cast_ray(self, ray_origin, direction, triangles, texture_path):
-        closest_t = float('inf')
-        closest_triangle = None
-
-        for triangle in triangles:
-            hit, t = self.does_ray_intersect(ray_origin, 0.001, direction, triangle)
-            if hit and t < closest_t:
-                closest_t = t
-                closest_triangle = triangle
-
-        if closest_triangle:
-            hit_point = ray_origin + closest_t * direction
-            return self.shade(hit_point, closest_triangle, texture_path)
-        else:
-            return (10, 10, 150)
-
-    def raytrace(self, matrix, texture_path):
-        all_tris = []
-        s = True
-        for matI in range(len(matrix)):
-            mat = matrix[matI]
-            if mat is None:
-                continue
-            #print(mat)
-            vertices, faces, uv, uv_faces = mat
-            for faceI in range(len(faces)):
-                face = faces[faceI]
-                p0 = vertices[face[0]]
-                p1 = vertices[face[1]]
-                p2 = vertices[face[2]]
-
-                if None in (p0, p1, p2):
-                    continue
-
-                near_plane = 0.01  # or something small
-                if max(p0[2], p1[2], p2[2]) <= near_plane:
-                    continue
-
-                all_tris.append((p0, p1, p2))
-        aspect = self.width / self.height
-        fov = math.radians(100)
-        fov_scale = math.tan(math.radians(fov / 2))
-
-        yaw   = self.camera.rotation[1]
-        pitch = self.camera.rotation[0]
-
-        forward = self.normalize(np.array([
-            math.cos(pitch) * math.sin(yaw),
-        -math.sin(pitch),
-            math.cos(pitch) * math.cos(yaw)
-        ]))
-        right = self.normalize(np.cross(forward, np.array([0, 1, 0])))
-        up    = np.cross(right, forward)
-        
-        cols = []
-
-        for yI in range(self.height):
-            c = []
-            for xI in range(self.width):
-                px = (2.0 * (xI + 0.5) / self.width  - 1.0) * fov_scale * aspect
-                py = (1.0 - 2.0 * (yI + 0.5) / self.height) * fov_scale
-                ray_origin = self.camera.position
-                ray_dir = self.normalize(forward + right * px + up * py)
-
-                col = self.cast_ray(ray_origin, ray_dir, all_tris, texture_path)
-                c.append(col)
-            cols.append(c)
-
-        surface_array = np.transpose(cols, (1, 0, 2))  # pygame wants (width, height, 3)
-        pygame.surfarray.blit_array(self.screen, surface_array)
-        pygame.display.flip()
 
 
     def normalize(self, v):
@@ -1131,8 +1006,6 @@ class Renderer3D:
                         self.projected_vertices_faces_list.append([projected, self.vertices_faces_list[i][1], self.vertices_faces_list[i][2], self.vertices_faces_list[i][3]])
                     #if not self.is_mesh:
                     self.render_shape_from_obj_format(self.projected_vertices_faces_list, self.texture_path)
-                elif self.render_type == renderer_type.RAYTRACE:
-                    self.raytrace(self.vertices_faces_list, self.texture_path)
 
             self.grid_coords_list = []
             self.triangle_color_list_1 = []
